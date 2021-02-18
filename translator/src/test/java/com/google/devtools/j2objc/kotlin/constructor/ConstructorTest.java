4995ee5a542150e981c59b798b363dddcdf1d730
Guillaume Lamontagne <glamontagne@mirego.com>
2021-02-18 10:34:32 -0500

    Generate a .h and a .m from a java source that has kotlin in it

---
+++ translator/src/test/java/com/google/devtools/j2objc/kotlin/constructor/ConstructorTest.java
@@ -0,0 +1,27 @@
+package com.google.devtools.j2objc.kotlin.constructor;
+
+import com.google.devtools.j2objc.GenerationTest;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ConstructorTest extends GenerationTest {
+
+    @Test
+    public void testClassWithoutConstructor() throws IOException {
+        String translation = translateSourceFile(
+                "import com.mirego.interop.kotlin.test.constructor.ClassWithoutConstructor;" +
+                        "class WithoutConstructor {" +
+                            "public WithoutConstructor() {}" +
+                            "public static ClassWithoutConstructor main(String[] args) {" +
+                                "final ClassWithoutConstructor withoutConstructor = new ClassWithoutConstructor();" +
+                                "return withoutConstructor;" +
+                            "}" +
+                        "}",
+                "WithoutConstructor", "WithoutConstructor.m"
+        );
+
+        System.out.println(translation);
+    }
+}
