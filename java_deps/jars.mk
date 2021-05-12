c12e6e6ad83e029bd181246dc8d74a2314af50cd
Guillaume Audet <guillaume.audet@gmail.com>
2021-05-11 21:27:29 -0400

    Update to Kotlin 1.5.0

--- java_deps/jars.mk
+++ java_deps/jars.mk
@@ -54,8 +54,8 @@ SCENELIB_JAR = scenelib.jar
 PLUME_UTIL_JAR = plume-util-1.0.6.jar
 
 # mirego kotlin interop
-KOTLIN_STDLIB_JAR = kotlin-stdlib-1.4.32.jar
-KOTLINX_METADATA_JVM_JAR = kotlinx-metadata-jvm-0.2.0.jar
+KOTLIN_STDLIB_JAR = kotlin-stdlib-1.5.0.jar
+KOTLINX_METADATA_JVM_JAR = kotlinx-metadata-jvm-0.3.0.jar
 JETBRAIN_ANNOTATION_JAR = annotations-20.1.0.jar
 
 MOCKITO_JAR = mockito-core-2.23.4.jar
