650562cac907624402d3f1a38baa18fff17d015b
Guillaume Audet <gaudet@mirego.com>
2022-02-15 07:02:11 -0500

    Update kotlin to 1.6.10

--- java_deps/jars.mk
+++ java_deps/jars.mk
@@ -54,9 +54,9 @@ SCENELIB_JAR = scenelib.jar
 PLUME_UTIL_JAR = plume-util-1.0.6.jar
 
 # mirego kotlin interop
-KOTLIN_STDLIB_JAR = kotlin-stdlib-1.5.0.jar
-KOTLINX_METADATA_JVM_JAR = kotlinx-metadata-jvm-0.3.0.jar
-JETBRAIN_ANNOTATION_JAR = annotations-20.1.0.jar
+KOTLIN_STDLIB_JAR = kotlin-stdlib-1.6.10.jar
+KOTLINX_METADATA_JVM_JAR = kotlinx-metadata-jvm-0.4.0.jar
+JETBRAIN_ANNOTATION_JAR = annotations-23.0.0.jar
 
 MOCKITO_JAR = mockito-core-2.23.4.jar
 MOCKITO_SOURCE_JAR = mockito-core-2.23.4-sources.jar
